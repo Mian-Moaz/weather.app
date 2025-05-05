88e9ad78dab880a1b43f6290c91f096156a54402
Mian-Moaz <muhammadmoaz749@gmail.com>
2025-05-04 20:50:33 -0700

    update changes

--- service/weatherApi.js
+++ service/weatherApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_KEY = process.env.NEXT_PUBLIC_WEATHER_API_KEY;
 // const BASE = 'https://api.openweathermap.org/data/2.5';
-const BASE = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=`;
+const BASE = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=`;
 
 export const fetchWeatherData = async (city) => {
   const res = await axios.get(`${BASE + city}`);
